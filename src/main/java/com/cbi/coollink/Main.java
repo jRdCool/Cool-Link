23cbba039af43d2050d603800aea4971d43a3cba
jSdCool <37940266+jSdCool@users.noreply.github.com>
2022-12-02 18:58:25 -0500

    added test block

--- src/main/java/com/cbi/coollink/Main.java
+++ src/main/java/com/cbi/coollink/Main.java
@@ -1,13 +1,24 @@
 package com.cbi.coollink;
 
+import com.cbi.coollink.blocks.TestBlock;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
+
 public class Main implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("cool-link");
+
     @Override
     public void onInitialize() {
         LOGGER.info("loading cool link");
+        Registry.register(Registry.BLOCK, new Identifier("cool-link","test_block"),TestBlock.ENTRY);
+        Registry.register(Registry.ITEM, new Identifier("cool-link", "test_block"),new BlockItem(TestBlock.ENTRY, new FabricItemSettings().group(ItemGroup.MISC)));
     }
 }
---
+++ src/main/java/com/cbi/coollink/blocks/TestBlock.java
@@ -0,0 +1,12 @@
+package com.cbi.coollink.blocks;
+
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+
+public class TestBlock extends Block {
+    public static final TestBlock ENTRY = new TestBlock(FabricBlockSettings.of(Material.STONE).hardness(4.0f));
+    public TestBlock(Settings settings) {
+        super(settings);
+    }
+}
