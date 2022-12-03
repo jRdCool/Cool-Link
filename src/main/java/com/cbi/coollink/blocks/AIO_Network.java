d1c835e1e02b62010b7111f9552047346c8c947b
jRdCool <joeyrduffy@gmail.com>
2022-12-02 19:59:19 -0500

    AIO Network Block added

--- src/main/java/com/cbi/coollink/blocks/AIO_Network.java
+++ src/main/java/com/cbi/coollink/blocks/AIO_Network.java
@@ -1,37 +1,26 @@
 // Made with Blockbench 4.5.2
-// Exported for Minecraft version 1.17 - 1.18 with Mojang mappings
+// Exported for Minecraft version 1.17+ for Yarn
 // Paste this class into your mod and generate all required imports
-
-
-public class AIO_Network<T extends Entity> extends EntityModel<T> {
-	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
-	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "aio_network"), "main");
+public class AIO_Network extends EntityModel<Entity> {
 	private final ModelPart bb_main;
-
 	public AIO_Network(ModelPart root) {
 		this.bb_main = root.getChild("bb_main");
 	}
-
-	public static LayerDefinition createBodyLayer() {
-		MeshDefinition meshdefinition = new MeshDefinition();
-		PartDefinition partdefinition = meshdefinition.getRoot();
-
-		PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main", CubeListBuilder.create().texOffs(0, 0).addBox(-6.0F, -2.0F, -6.0F, 12.0F, 2.0F, 12.0F, new CubeDeformation(0.0F))
-		.texOffs(0, 15).addBox(-7.0F, -4.0F, -7.0F, 14.0F, 2.0F, 14.0F, new CubeDeformation(0.0F))
-		.texOffs(0, 0).addBox(-4.5F, -2.5F, -6.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
-		.texOffs(0, 0).addBox(-1.5F, -2.5F, -6.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
-		.texOffs(0, 5).addBox(3.5F, -1.5F, -7.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
-
-		return LayerDefinition.create(meshdefinition, 64, 32);
+	public static TexturedModelData getTexturedModelData() {
+		ModelData modelData = new ModelData();
+		ModelPartData modelPartData = modelData.getRoot();
+		ModelPartData bb_main = modelPartData.addChild("bb_main", ModelPartBuilder.create().uv(0, 0).cuboid(-6.0F, -2.0F, -6.0F, 12.0F, 2.0F, 12.0F, new Dilation(0.0F))
+		.uv(0, 15).cuboid(-7.0F, -4.0F, -7.0F, 14.0F, 2.0F, 14.0F, new Dilation(0.0F))
+		.uv(0, 0).cuboid(-4.5F, -2.5F, -6.0F, 2.0F, 2.0F, 2.0F, new Dilation(0.0F))
+		.uv(0, 0).cuboid(-1.5F, -2.5F, -6.0F, 2.0F, 2.0F, 2.0F, new Dilation(0.0F))
+		.uv(0, 5).cuboid(3.5F, -1.5F, -7.0F, 1.0F, 1.0F, 1.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
+		return TexturedModelData.of(modelData, 64, 32);
 	}
-
 	@Override
-	public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-
+	public void setAngles(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
 	}
-
 	@Override
-	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-		bb_main.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+	public void render(MatrixStack matrices, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha) {
+		bb_main.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
 	}
 }
