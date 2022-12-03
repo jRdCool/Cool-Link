7179daa1528f714d677bb74fcfdbc24c367e05be
jSdCool <37940266+jSdCool@users.noreply.github.com>
2022-12-03 16:53:51 -0500

    add client entry point

---
+++ src/main/java/com/cbi/coollink/ClientEntryPoint.java
@@ -0,0 +1,10 @@
+package com.cbi.coollink;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClientEntryPoint implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        //register client things here
+    }
+}
