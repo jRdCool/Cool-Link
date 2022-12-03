e06269f673d5a770b21fd06196e0cf4a59a70fc6
jRdCool <joeyrduffy@gmail.com>
2022-12-02 22:22:59 -0500

    AIO Network rotated voxels

--- src/main/java/com/cbi/coollink/blocks/AIOBlockEntity.java
+++ src/main/java/com/cbi/coollink/blocks/AIOBlockEntity.java
@@ -5,7 +5,7 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.util.math.BlockPos;
 
 public class AIOBlockEntity extends BlockEntity {
-    public DAIOBlockEntity(BlockPos pos, BlockState state) {
-        super(ExampleMod.AIO_BLOCK_ENTITY, pos, state);
+    public AIOBlockEntity(BlockPos pos, BlockState state) {
+        super(coollink.AIO_BLOCK_ENTITY, pos, state);
     }
 }
