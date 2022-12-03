0c726db72120b528b799f7b5cba35161a8eefa54
jRdCool <joeyrduffy@gmail.com>
2022-12-03 15:43:03 -0500

    new gui for Smartphone added

---
+++ src/main/java/com/cbi/coollink/guis/PhoneScreen.java
@@ -0,0 +1,10 @@
+package com.cbi.coollink.guis;
+
+import io.github.cottonmc.cotton.gui.GuiDescription;
+import io.github.cottonmc.cotton.gui.client.CottonClientScreen;
+
+public class PhoneScreen extends CottonClientScreen {
+    public PhoneScreen(GuiDescription description) {
+        super(description);
+    }
+}
