0fa608044203275ba1500e4c545b988ea9adc3f7
jRdCool <joeyrduffy@gmail.com>
2022-12-02 22:34:40 -0500

    AIO Network rotated voxels complete

--- src/main/java/com/cbi/coollink/blocks/AIO_Network.java
+++ src/main/java/com/cbi/coollink/blocks/AIO_Network.java
@@ -61,7 +61,7 @@ public class AIO_Network extends Block{
 		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.53125, 0.03125, 0.75, 0.53125, 0.125, 0.875));
 		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.40625, 0.03125, 0.75, 0.40625, 0.125, 0.875));
 		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.40625, 0.03125, 0.75, 0.53125, 0.125, 0.75));
-		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.725, 0.0375, 0.875, 0.775, 0.0875, 0.96875));
+		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.725, 0.0375, 0.875, 0.775, 0.0875, 0.925));
 
 		return shape;
 	}
