ff89087b7dfdfc500b3f68128eab67e31eccfd7a
jRdCool <joeyrduffy@gmail.com>
2022-12-03 22:15:42 -0500

    Smartphone item updated

--- src/main/java/com/cbi/coollink/guis/PhoneGui.java
+++ src/main/java/com/cbi/coollink/guis/PhoneGui.java
@@ -1,10 +1,9 @@
 package com.cbi.coollink.guis;
 
+import io.github.cottonmc.cotton.gui.client.BackgroundPainter;
 import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
 import io.github.cottonmc.cotton.gui.widget.*;
-import io.github.cottonmc.cotton.gui.widget.icon.Icon;
 import io.github.cottonmc.cotton.gui.widget.icon.TextureIcon;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralTextContent;
 import net.minecraft.text.MutableText;
 import net.minecraft.util.Identifier;
@@ -12,7 +11,7 @@ import net.minecraft.util.Identifier;
 public class PhoneGui extends LightweightGuiDescription {
     public PhoneGui(){
         MutableText netButtonLabel = MutableText.of(new LiteralTextContent("Network Router"));
-        WGridPanel root = new WGridPanel();
+        WGridPanel root = new WGridPanel();//.setBackgroundPainter(new BackgroundPainter());
         setRootPanel(root);
         root.setSize(300,250);
         WButton accessPointButton = new WButton().setIcon(new TextureIcon(new Identifier("cool-link","textures/icon/cbi.png")));
