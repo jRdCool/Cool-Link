a2210f8cd3b4905e0493561704a95dcdefdf8d16
jSdCool <37940266+jSdCool@users.noreply.github.com>
2023-11-07 22:04:18 -0500

    coax cable voxles and particle

--- src/main/java/com/cbi/coollink/blocks/cables/CoaxCable.java
+++ src/main/java/com/cbi/coollink/blocks/cables/CoaxCable.java
@@ -1,11 +1,15 @@
 package com.cbi.coollink.blocks.cables;
 
-import com.cbi.coollink.items.CoaxialCable;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 
 public class CoaxCable extends Block {
 
@@ -37,5 +41,30 @@ public class CoaxCable extends Block {
         stateManager.add(up);
         stateManager.add(down);
 
+    }
+    @SuppressWarnings("deprecation")
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+        VoxelShape shape = VoxelShapes.empty();
+        shape = VoxelShapes.union(shape,VoxelShapes.cuboid(0.4375,0.4375,0.4375,0.5625,0.5625,0.5625));
+        if(state.get(north)){
+            shape = VoxelShapes.union(shape,VoxelShapes.cuboid(0.4375,0.4375,0,0.5625,0.5625,0.4375));
+        }
+        if(state.get(south)){
+            shape = VoxelShapes.union(shape,VoxelShapes.cuboid(0.4375,0.4375,0.5625,0.5625,0.5625,1));
+        }
+        if(state.get(east)){
+            shape = VoxelShapes.union(shape,VoxelShapes.cuboid(0.5625,0.4375,0.4375,1,0.5625,0.5625));
+        }
+        if(state.get(west)){
+            shape = VoxelShapes.union(shape,VoxelShapes.cuboid(0,0.4375,0.4375,0.4375,0.5625,0.5625));
+        }
+        if(state.get(up)){
+            shape = VoxelShapes.union(shape,VoxelShapes.cuboid(0.4375,0.5625,0.4375,0.5625,1,0.5625));
+        }
+        if(state.get(down)){
+            shape = VoxelShapes.union(shape,VoxelShapes.cuboid(0.4375,0,0.4375,0.5625,0.4375,0.5625));
+        }
+        return shape;
     }
 }
