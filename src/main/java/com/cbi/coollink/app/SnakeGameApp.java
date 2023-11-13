ef7a0f76f806445ad106a5d211f8fad97ff933a7
jSdCool <37940266+jSdCool@users.noreply.github.com>
2023-11-13 14:02:34 -0500

    work on nsake game

--- src/main/java/com/cbi/coollink/app/SnakeGameApp.java
+++ src/main/java/com/cbi/coollink/app/SnakeGameApp.java
@@ -1,6 +1,5 @@
 package com.cbi.coollink.app;
 
-import com.cbi.coollink.Main;
 import io.github.cottonmc.cotton.gui.client.ScreenDrawing;
 import io.github.cottonmc.cotton.gui.widget.WPlainPanel;
 import net.minecraft.block.entity.BlockEntity;
@@ -9,8 +8,17 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 
+import java.util.ArrayList;
+
 public class SnakeGameApp extends AbstractPhoneApp{
     static final Identifier appID = new Identifier("cool-link","snake-app");
+
+    ArrayList<int[]> snake = new ArrayList<>();
+    ArrayList<int[]> apples = new ArrayList<>();
+    int directionOfTravel,nextDirection;
+    int tickNum;
+
+    boolean gameOver =false;
     public SnakeGameApp() {//constructor used to create a dummy instance of the class used for app registration
         super(appID);//the id of the app
 
@@ -25,12 +33,18 @@ public class SnakeGameApp extends AbstractPhoneApp{
         super(appID);//the id of the app
         root=new WPlainPanel();//create the panel witch all widget will sit on
         timeColor=TIME_COLOR_BLACK;//set the color of the clock if necessary
-
+        snake.add(new int[]{7,7});
 
     }
     @Override
     public void tick() {
-
+        tickNum++;
+        if(!gameOver) {
+            if (tickNum % 10 == 0) {
+                directionOfTravel=nextDirection;
+                move1();
+            }
+        }
     }
 
     @Override
@@ -50,6 +64,57 @@ public class SnakeGameApp extends AbstractPhoneApp{
                     ScreenDrawing.coloredRect(matrices,left+i*cellSize+start,top+j*cellSize,cellSize,cellSize,((i+j)%2==0)?0xFF_00FF00:0xFF_00AA00);
                 }
             }
+
+            for (int[] pos : snake) {
+                ScreenDrawing.coloredRect(matrices,left+pos[0]*cellSize+start,top+pos[1]*cellSize,cellSize,cellSize,0xFF_00A0FF);
+            }
         });
+    }
+    /*directions:
+    0 = up
+    1 = left
+    2 = down
+    3 = right
+
+     */
+    void move1(){
+        int[] curPos = snake.get(snake.size()-1);
+        int[] newPos;
+        switch (directionOfTravel){
+            case 0 -> newPos = new int[]{curPos[0],curPos[1]-1};
+            case 1 -> newPos = new int[]{curPos[0]-1,curPos[1]};
+            case 2 -> newPos = new int[]{curPos[0],curPos[1]+1};
+            case 3 -> newPos = new int[]{curPos[0]+1,curPos[1]};
+            default -> newPos = new int[]{0,0};
+        }
+        if(newPos[0] < 0 || newPos[0]>14 || newPos[1] < 0 || newPos[1]>14){
+            gameOver = true;
+            return;
+        }
+        snake.add(newPos);
+        snake.remove(0);
+    }
+
+    /*
+    265         328        UP
+    263         331        LEFT
+    264         336        DOWN
+    262         333        RIGHT
+     */
+    @Override
+    public void keyPressed(int ch, int keyCode, int modifiers) {
+        if(ch == 265){
+            if(directionOfTravel!=2)
+                nextDirection=0;
+        }else if(ch == 263){
+            if(directionOfTravel!=3)
+               nextDirection=1;;
+        }else if(ch ==264){
+            if(directionOfTravel!=0)
+               nextDirection=2;
+        }else if(ch == 262){
+            if(directionOfTravel!=1)
+               nextDirection=3;
+        }
     }
 }
