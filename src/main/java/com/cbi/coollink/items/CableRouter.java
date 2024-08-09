66824637921deed403b359e5525c44e24ffc28f7
jRdCool <joeyrduffy@gmail.com>
2024-08-09 15:17:16 -0400

    started and branched mac addresses

---
+++ src/main/java/com/cbi/coollink/items/CableRouter.java
@@ -0,0 +1,9 @@
+package com.cbi.coollink.items;
+
+import net.minecraft.item.Item;
+
+public class CableRouter extends Item {
+    public CableRouter(Settings settings) {super(settings);}
+
+
+}
---
+++ src/main/java/com/cbi/coollink/net/protocol/Mac.java
@@ -0,0 +1,22 @@
+package com.cbi.coollink.net.protocol;
+
+public class Mac {
+
+    private int[] mac;
+    private Mac maac;
+
+
+    public Mac(int deviceType){
+
+        mac = new int[3];
+
+
+    }
+
+    public Mac generateMacAddress(){
+      return maac;
+    }
+
+
+
+}
