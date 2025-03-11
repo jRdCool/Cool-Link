841a1bc3e4df707200942ad5961e4152f6e5a11f
jRdCool <joeyrduffy@gmail.com>
2025-03-10 23:37:34 -0400

    Cable connection info now displays in the action bar

--- src/main/java/com/cbi/coollink/items/ACableItem.java
+++ src/main/java/com/cbi/coollink/items/ACableItem.java
@@ -9,6 +9,7 @@ import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.network.packet.s2c.play.OverlayMessageS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
@@ -30,13 +31,16 @@ public class ACableItem extends Item {
         ItemStack stack = context.getStack();
 
 
-        if(world.getBlockEntity(pos) instanceof IWireNode node){
-            Main.LOGGER.info("Clicked on an IWire node");
-            if(stack.contains(Main.WIRE_INFO_COMPONENT)) {
+        if(world.getBlockEntity(pos) instanceof IWireNode node) {
+            //Main.LOGGER.info("Clicked on an IWire node");
+            if (stack.contains(Main.WIRE_INFO_COMPONENT)) {
                 WireInfoComponent comp = stack.get(Main.WIRE_INFO_COMPONENT);
 
-                if (comp!=null&&(!comp.originBlock().isWithinDistance(pos, 16))) {
-                    if (context.getPlayer() instanceof ServerPlayerEntity player) {player.sendMessageToClient(Text.of("!!Nodes are too far apart. Aborting!!"), false);}
+                if (comp != null && (!comp.originBlock().isWithinDistance(pos, 16))) {
+                    if (context.getPlayer() instanceof ServerPlayerEntity player) {
+                        //player.sendMessageToClient(Text.of("!!Nodes are too far apart. Aborting!!"), false);
+                        player.networkHandler.sendPacket(new OverlayMessageS2CPacket(Text.of("!!Nodes are too far apart. Aborting!!")));
+                    }
                     stack.remove(Main.WIRE_INFO_COMPONENT);
                     return super.useOnBlock(context);
                 }
@@ -44,45 +48,53 @@ public class ACableItem extends Item {
             int index = 0;
             int nodeCount = node.getNodeCount();
             ArrayList<Integer> ofType = new ArrayList<>();
-            for(int i=0;i< nodeCount;i++){
-                if(node.getPortType(i)==TYPE){
+            for (int i = 0; i < nodeCount; i++) {
+                if (node.getPortType(i) == TYPE) {
                     ofType.add(i);
                 }
             }
-            if(ofType.isEmpty()){
-                if(context.getPlayer() instanceof ServerPlayerEntity player){player.sendMessageToClient(Text.of("No " + TYPE.toString() + " ports on this device"),false);}
+            if (ofType.isEmpty()) {
+                if (context.getPlayer() instanceof ServerPlayerEntity player) {
+                    //player.sendMessageToClient(Text.of("No " + TYPE.toString() + " ports on this device"), false);
+                    player.networkHandler.sendPacket(new OverlayMessageS2CPacket(Text.of("No " + TYPE.toString() + " ports on this device")));
+                }
                 return super.useOnBlock(context);
             }
-            if(ofType.size()>1){
-                if(context.getPlayer() instanceof ServerPlayerEntity sp) {
+            if (ofType.size() > 1) {
+                if (context.getPlayer() instanceof ServerPlayerEntity sp) {
                     ServerPlayNetworking.send(sp, new OpenPortSelectGuiPacket(ofType, TYPE, context.getWorld().getRegistryKey(), context.getBlockPos(), context.getStack()));
                 }
                 return super.useOnBlock(context);
-            }else if (nodeCount>1){
+            } else if (nodeCount > 1) {
                 index = ofType.get(0);
             }
-            if(world.getBlockEntity(pos) instanceof IWireNode tb&& tb.isNodeInUse(index)){return super.useOnBlock(context);}
+            if (world.getBlockEntity(pos) instanceof IWireNode tb && tb.isNodeInUse(index)) {
+                return super.useOnBlock(context);
+            }
 
-            if(stack.contains(Main.WIRE_INFO_COMPONENT)){
+            if (stack.contains(Main.WIRE_INFO_COMPONENT)) {
                 Main.LOGGER.info("Info component present");
                 WireInfoComponent comp = stack.get(Main.WIRE_INFO_COMPONENT);
                 //Main.LOGGER.info(comp.originBlock().toString());
-                if(comp!=null&&world.getBlockEntity(comp.originBlock()) instanceof IWireNode block){
-                   block.setNode(comp.index(),index,pos,TYPE);
-                    block.setNode(comp.index(),index,block.getPos(),TYPE);
-                    node.setNode(index,comp.index(),comp.originBlock(),TYPE);
+                if (comp != null && world.getBlockEntity(comp.originBlock()) instanceof IWireNode block) {
+                    block.setNode(comp.index(), index, pos, TYPE);
+                    block.setNode(comp.index(), index, block.getPos(), TYPE);
+                    node.setNode(index, comp.index(), comp.originBlock(), TYPE);
                     //Main.LOGGER.info("Rendering");
                     //Main.LOGGER.info(block.getNodeOffset(comp.index())+"");}
                 }
 
                 stack.remove(Main.WIRE_INFO_COMPONENT);
-            }
-            else {
+            } else {
                 stack.set(Main.WIRE_INFO_COMPONENT, new WireInfoComponent(index, pos));
+
+                if (context.getPlayer() instanceof ServerPlayerEntity player) {
+                    player.networkHandler.sendPacket(new OverlayMessageS2CPacket(Text.of("Starting "+ TYPE.toString()+" Connection at: "+ pos.getX()+", "+pos.getY()+", "+pos.getZ())));
+                }
                 //Main.LOGGER.info("Stack Component Set");
                 //Main.LOGGER.info(stack.get(Main.WIRE_INFO_COMPONENT).toString());
             }
-            if(context.getPlayer()!= null) context.getPlayer().swingHand(context.getHand());
+            if (context.getPlayer() !=null) {context.getPlayer().swingHand(context.getHand());}
         }
 
 
