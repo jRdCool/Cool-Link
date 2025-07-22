cded48f8a0a559563a651e6d457a0a1cd6b0a977
jSdCool <jSdCoolgaming@gmail.com>
2025-07-21 23:59:43 -0400

    added send packet and wait command to the terminal also packet responses on device network managers should work TM

---
+++ src/main/java/com/cbi/coollink/cli/lowlevelnet/SendPackAndWaitCommand.java
@@ -0,0 +1,68 @@
+package com.cbi.coollink.cli.lowlevelnet;
+
+import com.cbi.coollink.Util;
+import com.cbi.coollink.cli.CliProgram;
+import com.cbi.coollink.net.protocol.ProgramNetworkInterface;
+import com.cbi.coollink.terminal.CommandTextOutputArea;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.StringNbtReader;
+
+import java.util.HashMap;
+
+public class SendPackAndWaitCommand implements CliProgram {
+
+    boolean response = false;
+
+    public SendPackAndWaitCommand(String[] args, HashMap<String,String> ignoredEnv, CommandTextOutputArea stdout, ProgramNetworkInterface networkInterface){
+        if(args.length<2){
+            stdout.addLine("Invalid args, expected sendpack <ip> <NBTdata>");
+            response = true;
+            return;
+        }
+        String destIp = args[0];
+        if(!Util.validIp(destIp)){
+            stdout.addLine("Invalid Ip");
+            response = true;
+            return;
+        }
+
+        if(!networkInterface.isConnectedToNetwork()){
+            stdout.addLine("Not connected to network");
+        }
+
+        String[] nbtArgs = new String[args.length-1];
+        System.arraycopy(args,1,nbtArgs,0,nbtArgs.length);
+        String rawStringNbt = String.join(" ",nbtArgs);
+        NbtCompound packetNbt;
+        try {
+            packetNbt = StringNbtReader.readCompound(rawStringNbt);
+        } catch (CommandSyntaxException e) {
+            stdout.addLine("§cError parsing NBT:§r "+e.getMessage());
+            response = true;
+            return;
+        }
+        networkInterface.sendRawData(destIp,packetNbt, context -> {
+            stdout.addLine("Received response from: " + context.getData().getSourceIpAddress() + " " + context.getData().getData());
+            response = true;
+        });
+    }
+
+    /**
+     * Process ongoing program operations
+     */
+    @Override
+    public void tick() {
+
+    }
+
+    /**
+     * Get if this program is still running
+     *
+     * @return true if this program is still running on the tick method
+     */
+    @Override
+    public boolean isProgramRunning() {
+        return !response;
+    }
+}
--- src/main/java/com/cbi/coollink/net/protocol/ProgramNetworkInterface.java
+++ src/main/java/com/cbi/coollink/net/protocol/ProgramNetworkInterface.java
@@ -1,6 +1,9 @@
 package com.cbi.coollink.net.protocol;
 
+import com.cbi.coollink.Util;
 import net.minecraft.nbt.NbtCompound;
+
+import java.util.HashMap;
 
 abstract public class ProgramNetworkInterface {
     private final Mac deviceMac;
@@ -8,14 +11,45 @@ abstract public class ProgramNetworkInterface {
 
     private PacketReceiver packetReceiver;
 
+    private final HashMap<Integer,PackerResponse> responseMap = new HashMap<>();
+
 
     public ProgramNetworkInterface(Mac deviceMacAddress, String deviceIpAddress){
         deviceIp = deviceIpAddress;
         deviceMac =deviceMacAddress;
     }
 
+    /**Send a packet to the specified ip address
+     * @param ipAddress The Ip to send the data to
+     * @param sendData The data to send to the other device
+     */
     public void sendRawData(String ipAddress, NbtCompound sendData){
         IpDataPacket data = new IpDataPacket(ipAddress,deviceIp,deviceMac,sendData);
+        sendIpPacketOverNetwork(data);
+    }
+
+    /**Send a packet to the specified ip address on a random port and listed for a response on that port
+     * @param ipAddress The Ip to send the data to
+     * @param sendData The data to send to the other device
+     * @param responseCallback The code to run when a response is received, note this call balk is single use
+     */
+    public void sendRawData(String ipAddress, NbtCompound sendData,PackerResponse responseCallback){
+        //check if a port was included
+        int port = -1;
+        while(port == -1){
+            //generate a random port
+            port = (int)(Math.random()*5900)+100;
+            //check that port is not in use allready
+            if(responseMap.containsKey(port)){
+                port = -1;
+            }
+        }
+
+        //register that port in the response map
+        responseMap.put(port,responseCallback);
+
+        //send the packet with the port in the ip
+        IpDataPacket data = new IpDataPacket(ipAddress,deviceIp+":"+port,deviceMac,sendData);
         sendIpPacketOverNetwork(data);
     }
 
@@ -26,7 +60,25 @@ abstract public class ProgramNetworkInterface {
     public abstract void sendIpPacketOverNetwork(IpDataPacket data);
 
     public void processReceivedDataPacket(IpDataPacket receivedData){
-        if(packetReceiver != null){
+        //when a new packet comes in
+
+        //update the internal ip to mac cash
+
+        //check if the port responded on has a handler associated with it
+        int responsePort = Util.parseIpGetPort(receivedData.getDestinationIpAddress());
+        if(responsePort != -1) {//if -1 then no port was included so just go to the normal handler
+            PackerResponse responseHandler = responseMap.get(responsePort);
+            if (responseHandler != null) {//if a handler for this port was found
+                responseMap.remove(responsePort);//remove this so further packets on this port won't accidentally trigger this code again
+                responseHandler.responseReceived(new ResponseContext(responsePort, receivedData));//activate that handler
+            } else {//otherwise
+                //just do the normal response handler
+                if (packetReceiver != null) {
+                    packetReceiver.receive(receivedData);
+                }
+            }
+        }
+        if (packetReceiver != null) {
             packetReceiver.receive(receivedData);
         }
     }
@@ -36,12 +88,16 @@ abstract public class ProgramNetworkInterface {
     }
 
     public void clearPacketReceivers(){
-        //NOTE: this will also have to delete any ports that are being listened on
         packetReceiver = null;
+        responseMap.clear();
     }
 
     public interface PacketReceiver{
         void receive(IpDataPacket receivedData);
+    }
+
+    public interface PackerResponse{
+        void responseReceived(ResponseContext context);
     }
 
     public abstract boolean isConnectedToNetwork();
@@ -51,4 +107,34 @@ abstract public class ProgramNetworkInterface {
     public String getDeviceIp(){
         return deviceIp;
     };
+
+    public class ResponseContext{
+        private final int port;
+        private final IpDataPacket responseData;
+
+        private ResponseContext(int port, IpDataPacket responseData){
+            this.responseData = responseData;
+            this.port=port;
+        }
+
+        /**Respond to the incoming data and do not listen for another response
+         * @param data The data to respond with
+         */
+        public void respond(NbtCompound data){
+            sendIpPacketOverNetwork(responseData.createResponsePacket(data));
+        }
+
+        /**Respond to the incoming data and listen for another response
+         * @param data The data to respond with
+         * @param callback The code to run  when the next response is received, note this call back is single use
+         */
+        public void respond(NbtCompound data, PackerResponse callback){
+            responseMap.put(port,callback);
+            sendIpPacketOverNetwork(responseData.createResponsePacket(data));
+        }
+
+        public IpDataPacket getData(){
+            return responseData;
+        }
+    }
 }
